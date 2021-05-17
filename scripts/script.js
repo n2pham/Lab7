d25be080593590dba3985a42cf1841775b451561
Nhan Pham <n2pham@ucsd.edu>
2021-05-16 19:45:45 -0700

    part2

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,89 @@
 // script.js
 
-import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
+import { router } from "./router.js"; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
 
 // Make sure you register your service worker here too
-
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('https://cse110lab6.herokuapp.com/entries')
-    .then(response => response.json())
-    .then(entries => {
-      entries.forEach(entry => {
-        let newPost = document.createElement('journal-entry');
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/sw.js").then(
+      function (registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
+// Make sure you register your service worker here too
+//added
+let setting = document.getElementsByTagName("img")[0];
+let main = document.querySelector("main");
+let title = document.querySelector("h1");
+let body = document.querySelector("body");
+let count = 1;
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://cse110lab6.herokuapp.com/entries")
+    .then((response) => response.json())
+    .then((entries) => {
+      entries.forEach((entry) => {
+        let newPost = document.createElement("journal-entry");
         newPost.entry = entry;
-        document.querySelector('main').appendChild(newPost);
+        newPost.number = count;
+
+        newPost.addEventListener("click", () => {
+          router.setState(
+            newPost.entry,
+            "Entry " + newPost.number,
+            "#" + newPost.number
+          );
+          main.style.display = "none";
+
+          //changing the title
+          title.innerText = "Entry " + newPost.number;
+
+          //delete the old entry page
+          let entry_page = document.querySelector("entry-page");
+          if (entry_page) {
+            entry_page.remove();
+          }
+
+          //create the new entry page
+          let new_entry = document.createElement("entry-page");
+          new_entry.entry = newPost.entry;
+          new_entry.style.display = "block";
+
+          let position = document.querySelector("body");
+          position.appendChild(new_entry);
+        });
+
+        count++;
+        document.querySelector("main").appendChild(newPost);
       });
     });
+
+  setting.addEventListener("click", () => {
+    router.setState({ index: "setting" }, "Setting ", "#settings");
+    main.style.display = "none";
+    title.innerText = "Settings";
+    body.classList.add("settings");
+    //delete the old entry page
+    let entry_page = document.querySelector("entry-page");
+    if (entry_page) {
+      entry_page.remove();
+    }
+  });
+
+  title.addEventListener("click", () => {
+    router.setState({ index: "home" }, "Home Page ", "/");
+    main.style.display = "block";
+    body.classList.remove("settings");
+    title.innerText = "Journal Entries";
+  });
 });
--- sw.js
+++ sw.js
@@ -4,3 +4,34 @@
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+const CACHE_NAME = "my-site-cache-v1";
+const CACHE_URL = ["https://cse110lab6.herokuapp.com/entries"];
+
+// Install event listener
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(CACHE_URL);
+    })
+  );
+});
+
+// Activation event listener
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Fetch event listener
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
